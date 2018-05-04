09325266162c06fbcf5469c26916d922733d2273
Angelina Davies CPA, CGA, MBA, IFRS Certified AICPA <35665531+angelyna@users.noreply.github.com>
2018-05-04 05:56:27 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,23 +4,72 @@ rAttempts = Array ("Remaining Attempts: 10", "Remaining Attempts: 9",
 "Remaining Attempts: 2", "Remaining Attempts: 1", 
 'Attempts Exhausted! Touch any Key to Reveal the Famous Singer OR Press ReStart')
 
-Singers = Array("Adele", "Tina", "Celina", "Rhianna", "Elvis", "whitney", "Mariah")
+Singers = [
+    {name:"Adele",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Adele_2016.jpg/128px-Adele_2016.jpg"
+    },
+    
+    {name:"Rhianna",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Rihanna_concert_in_Washington_DC_%282%29.jpg/128px-Rihanna_concert_in_Washington_DC_%282%29.jpg"
+    },
+
+    {name:"Celine",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Celine_Dion_Concert_-_Singing_I%27m_Alive.jpg/128px-Celine_Dion_Concert_-_Singing_I%27m_Alive.jpg"
+    },
+
+    {name:"Sarah",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Osaka07_Opening_Sarah_Brightman.jpg/128px-Osaka07_Opening_Sarah_Brightman.jpg"
+    },
+
+    {
+    name:"Whitney",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flickr_Whitney_Houston_performing_on_GMA_2009_4.jpg/128px-Flickr_Whitney_Houston_performing_on_GMA_2009_4.jpg"
+    },
+
+    {name:"Elton",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Elton_John_2011_Shankbone_2.JPG/128px-Elton_John_2011_Shankbone_2.JPG"
+    },
+
+    {name:"Justin",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Justin_Bieber_in_2015.jpg/128px-Justin_Bieber_in_2015.jpg"
+    },
+
+    {
+    name: "Elvis",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Elvis_Presley_Jailhouse_Rock.jpg/128px-Elvis_Presley_Jailhouse_Rock.jpg"    
+    },
+
+    {name: "Mariah",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/MariahGMA.jpg/128px-MariahGMA.jpg"
+    },
+
+    {
+    name: "Shakira",
+    src:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Shakira_at_Obama_Inaugural_%28cropped%29.jpg/128px-Shakira_at_Obama_Inaugural_%28cropped%29.jpg"    
+    }
+]
 
 function resetGame() {
  attempts = 0
  max = rAttempts.length
  letter = " "
- theSinger = Singers[Math.round((Singers.length - 1)*Math.random())].toUpperCase()
+ var remove = document.getElementById('img')
+ remove.removeChild(remove.childNodes[0])
+ randomno = Singers[Math.round((Singers.length - 1)*Math.random())]
+ theSinger = randomno.name.toUpperCase()
+ thePic = randomno.src
  displayAttempts()
  displaySinger()
  displayLetters()
+ var x = document.getElementById(thePic);
+ x.remove(x.thePic);
 }
 
 function displayAttempts() {
  document.SingersGame.status.value=rAttempts[attempts]
 }
 
-function displaySinger() {
+function displaySinger() {   
  hide=""
  for(i=0;i<theSinger.length;i++) {
   if(letter.indexOf(theSinger.charAt(i)) != -1)
@@ -29,9 +78,11 @@ function displaySinger() {
 }
 document.SingersGame.theSinger.value=hide
 }
+
 function displayLetters() {
  document.SingersGame.letter.value=letter 
 }
+
 function wrongLetter(s) {
  if(theSinger.indexOf(s) == -1) return true
  else
@@ -43,6 +94,13 @@ function winner() {
   if(letter.indexOf(theSinger.charAt(i)) == -1) return false
  }
  return true
+}
+
+function displayPicture() {
+    var x = document.createElement("img")
+    x.src = thePic
+    var src = document.getElementById("img")
+    src.appendChild(x)
 }
 
 function guess(k){
@@ -59,7 +117,6 @@ function guess(k){
 displaySinger()
 document.SingersGame.theSinger.value=theSinger
 alert("Congratulations!!! "+theSinger+" was the Famous Singer!")
+displayPicture()
  }
 }
-
-resetGame()
